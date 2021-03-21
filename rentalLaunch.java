4e9af4b26fcb8b6e169b76894f7f290caf2480f3
Adithi <68917173+AnuguAdithi@users.noreply.github.com>
2021-03-22 00:58:17 +0530

    Create rentalLaunch.java

---
+++ rentalLaunch.java
@@ -0,0 +1,11 @@
+package launch;
+import rental.*;
+public class rentalLaunch {
+
+	public static void main(String[] args) {
+		rentalManagament rmang=new rentalManagament();
+		rmang.setSize(500,500);
+		rmang.setVisible(true);
+	}
+
+}
